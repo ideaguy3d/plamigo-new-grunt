781e63c0d8f132abaabebaa6ed83f8d431aa2139
Julius Alvarado <javascript.uiux@gmail.com>
2019-05-08 09:44:08 -0700

    trying to get grunt-babel to work

---
+++ grunt-prac1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Julius Alvarado on 4/27/2018.
+ */
+
+module.exports = function(grunt){
+
+    grunt.initConfig();
+
+    grunt.loadNpmTasks('');
+
+    grunt.registerTask();
+
+};
